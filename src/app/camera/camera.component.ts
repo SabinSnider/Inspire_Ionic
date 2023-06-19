cc61604c91b80d8dc4626ffdb0dcdec6b11a8740
SabinSnider <sabinvsic@gmail.com>
2023-06-19 22:54:55 +0545

    Feat: Camera

--- src/app/camera/camera.component.ts
+++ src/app/camera/camera.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-camera',
   templateUrl: './camera.component.html',
   styleUrls: ['./camera.component.scss'],
 })
-export class CameraComponent  implements OnInit {
+export class CameraComponent implements OnInit {
+  picture: string | undefined;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
 
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 100,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Prompt
+    });
+    this.picture = image.dataUrl;
+  }
 }
